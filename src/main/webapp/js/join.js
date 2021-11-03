587f040bd12f06fece12259cdfaa62528003f94b
Hoyeon Jang <46728564+qkeb700@users.noreply.github.com>
2021-11-04 01:47:20 +0900

    finished join01.jsp
    done

--- src/main/webapp/js/join.js
+++ src/main/webapp/js/join.js
@@ -1,3 +1,21 @@
-/**
- * 
- */
+$(function(){
+	$('#all').change(function(){
+		if(this.checked){
+			$('.yak').prop("checked", true);
+		} else {
+			$('.yak').prop("checked", false);
+		}
+	})
+	$('#joinform').submit(function(){
+		if($('#yak01').is(':checked') == false){
+			alert("회원이용 약관에 동의해 주세요.");
+			$('#yak01').focus();
+			return false;
+		}
+		if($('#yak02').is(':checked') == false){
+			alert("개인정보 수집에 동의해 주세요.");
+			$('#yak02').focus();
+			return false;
+		}
+	})
+})
